1721c0c48f6c5bb173de961004c8271e04379ea2
Kingrosez <fairoz970a@gmail.com>
2025-04-09 11:30:09 +0530

    second

--- src/javaVariables/PrimitiveType.java
+++ src/javaVariables/PrimitiveType.java
@@ -4,6 +4,10 @@ public class PrimitiveType {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
