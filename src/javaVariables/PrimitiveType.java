0d6b676bb6df52a88dabe64341c898d9116adbb1
Kingrosez <fairoz970a@gmail.com>
2025-04-12 14:06:54 +0530

    thred

--- src/javaVariables/PrimitiveType.java
+++ src/javaVariables/PrimitiveType.java
@@ -2,12 +2,28 @@ package javaVariables;
 
 public class PrimitiveType {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
+		 byte b = 100;
+	        short s = 1000;
+	        int i = 100000;
+	        long l = 10000000000L;
+
+	        float f = 5.75f;
+	        double d = 19.99;
+
+	        char c = 'A';
+	        boolean bool = true;
+	        
+	        System.out.println(" this are primitive data types in java");
+	        System.out.println("byte b = 100;           it is one byte default value is 0 range is -128 - 127\r\n"
+	        		+ "short s = 1000;  it is one byte default value is 0 range is -32,768 to 32,767\r\n"
+	        		+ "int i = 100000;  it is one byte default value is 0 most commnly used in intiger type\r\n"
+	        		+ "long l = 10000000000L;  it is one byte default value is 0L Large integer, add L at the end\r\n"
+	        		+ "\r\n"
+	        		+ "float f = 5.75f; Single-precision decimal, add f\r\n"
+	        		+ "double d = 19.99; 	Double-precision decimal\r\n"
+	        		+ "\r\n"
+	        		+ "char c = 'A'; Single Unicode character\r\n"
+	        		+ "boolean bool = true; Only true or false || 1 or 1");
 	}
 
 }
